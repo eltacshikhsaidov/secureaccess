8c85b2850f6668d567f479ac53af1ac97f51db2d
eltac <eltac.shixseyidov250301@gmail.com>
2023-04-09 02:12:17 +0400

    added logging structure for email scheduler

--- src/main/java/io/shikhsaidov/secureaccess/job/EmailScheduler.java
+++ src/main/java/io/shikhsaidov/secureaccess/job/EmailScheduler.java
@@ -4,6 +4,7 @@ import io.shikhsaidov.secureaccess.entity.EmailInfo;
 import io.shikhsaidov.secureaccess.enums.EmailStatus;
 import io.shikhsaidov.secureaccess.repository.EmailInfoRepository;
 import io.shikhsaidov.secureaccess.service.EmailService;
+import io.shikhsaidov.secureaccess.util.LogDetail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -14,7 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static java.util.Objects.isNull;
 
 @Log4j2
 @Component
@@ -24,16 +24,35 @@ public class EmailScheduler {
 
     private final EmailInfoRepository emailInfoRepository;
     private final EmailService emailService;
+    private final LogDetail logDetail;
 
     @Scheduled(cron = "0 */2 * ? * *")
     @Transactional
-    public void retryConfirmationEmailSending() {
-        log.info("Retrying to send confirmation emails which is not sent");
+    public void retryEmailSending() {
+        log.info(
+                "requestPath: '{}', serverIp: '{}', [retryEmailSending] schedule function started",
+                logDetail.getRequestPath(),
+                logDetail.getIp()
+        );
         List<EmailInfo> emailInfoList = emailInfoRepository.getEmailInfoByStatus(EmailStatus.RETRY);
-        log.info("emailInfoList: {}", emailInfoList);
 
-        if (isNull(emailInfoList)) {
-            log.info("There is not any email with retry status");
+        String functionMessage = emailInfoList.size() == 0
+                ? "There is not any failed email"
+                : "emailInfoList: " + emailInfoList;
+        log.info(
+                "requestPath: '{}', serverIp: '{}', [retryEmailSending] " +
+                        "fetching failed emails response : {}",
+                logDetail.getRequestPath(),
+                logDetail.getIp(),
+                functionMessage
+        );
+
+        if (emailInfoList.size() == 0) {
+            log.info(
+                    "requestPath: '{}', serverIp: '{}', [retryEmailSending] schedule function ended",
+                    logDetail.getRequestPath(),
+                    logDetail.getIp()
+            );
             return;
         }
 
@@ -42,7 +61,13 @@ public class EmailScheduler {
 
                     try {
 
-                        log.info("Retrying to send confirmation email to: {}", emailInfo.emailTo);
+                        log.info(
+                                "requestPath: '{}', serverIp: '{}', " +
+                                        "[retryEmailSending] schedule function response: {}",
+                                logDetail.getRequestPath(),
+                                logDetail.getIp(),
+                                emailInfo.emailTo
+                        );
                         emailService.sendEmail(emailInfo);
 
                         emailInfo.setStatus(EmailStatus.SENT);
@@ -50,7 +75,15 @@ public class EmailScheduler {
                         emailInfo.setRetryCount(emailInfo.getRetryCount() + 1);
 
                     } catch (Exception e) {
-                        log.info("Exception occurred while sending an email: {}", e.getMessage());
+                        log.warn(
+                                "requestPath: '{}', serverIp: '{}', " +
+                                        "[retryEmailSending] schedule function response: {}, " +
+                                        "exception message: {}",
+                                logDetail.getRequestPath(),
+                                logDetail.getIp(),
+                                "Exception occurred while sending email to: " + emailInfo.emailTo,
+                                e.getMessage()
+                        );
                         emailInfo.setRetriedAt(LocalDateTime.now());
                         if (emailInfo.retryCount >= 3) {
                             emailInfo.setStatus(EmailStatus.FAILED);
@@ -71,6 +104,10 @@ public class EmailScheduler {
                 }
         );
 
-        log.info("retryConfirmationEmailSending ended");
+        log.info(
+                "requestPath: '{}', serverIp: '{}', [retryEmailSending] schedule function ended",
+                logDetail.getRequestPath(),
+                logDetail.getIp()
+        );
     }
 }
