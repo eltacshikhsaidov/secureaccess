76b303a31281f28b8707c9a0710a7cef053818b5
eltac <eltac.shixseyidov250301@gmail.com>
2023-04-09 01:34:45 +0400

    added log structure for logout service

--- src/main/java/io/shikhsaidov/secureaccess/service/LogoutService.java
+++ src/main/java/io/shikhsaidov/secureaccess/service/LogoutService.java
@@ -1,6 +1,7 @@
 package io.shikhsaidov.secureaccess.service;
 
 import io.shikhsaidov.secureaccess.repository.TokenRepository;
+import io.shikhsaidov.secureaccess.util.LogDetail;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,7 @@ import java.io.IOException;
 import static io.shikhsaidov.secureaccess.response.Response.response;
 import static io.shikhsaidov.secureaccess.response.ResponseCodes.SUCCESS;
 import static io.shikhsaidov.secureaccess.response.ResponseCodes.TOKEN_IS_INVALID_OR_EXPIRED;
+import static io.shikhsaidov.secureaccess.util.Utility.isNull;
 import static io.shikhsaidov.secureaccess.util.Utility.object2Json;
 
 @Log4j2
@@ -25,6 +27,7 @@ import static io.shikhsaidov.secureaccess.util.Utility.object2Json;
 public class LogoutService implements LogoutHandler {
 
     private final TokenRepository tokenRepository;
+    private final LogDetail logDetail;
 
     @Override
     public void logout(
@@ -34,12 +37,15 @@ public class LogoutService implements LogoutHandler {
     ) {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
-        if (authHeader == null ||!authHeader.startsWith("Bearer ")) {
-            log.info("Token is invalid");
-
-            String invalidTokenJsonResponse = object2Json(
-                    response(TOKEN_IS_INVALID_OR_EXPIRED)
+        if (isNull(authHeader) || !authHeader.startsWith("Bearer ")) {
+            log.warn(
+                    "requestPath: '{}', clientIp: '{}', function response: " +
+                            "token is not valid or expired",
+                    logDetail.getRequestPath(),
+                    logDetail.getIp()
             );
+
+            String invalidTokenJsonResponse = object2Json(response(TOKEN_IS_INVALID_OR_EXPIRED));
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
             response.setStatus(HttpStatus.BAD_REQUEST.value());
             try {
@@ -59,10 +65,12 @@ public class LogoutService implements LogoutHandler {
             SecurityContextHolder.clearContext();
         }
 
-        log.info("Successfully logged out!");
-        String logoutJsonResponse = object2Json(
-                response(SUCCESS)
+        log.info(
+                "requestPath: '{}', clientIp: '{}', function response: success",
+                logDetail.getRequestPath(),
+                logDetail.getIp()
         );
+        String logoutJsonResponse = object2Json(response(SUCCESS));
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpStatus.OK.value());
         try {
